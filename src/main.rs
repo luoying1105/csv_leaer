421c1b2f87cf61c15edecca8685dc6c343672e25
luoying <411426150@qq.com>
2024-04-22 15:34:43 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+
+
 use clap::Parser;
+use std::path::Path;
 #[derive(Parser,Debug)]
 #[command(name="rcli",version,about,long_about)]
 struct Opts {
@@ -15,11 +18,12 @@ enum SubCommands{
 
 #[derive(Parser,Debug)]
 struct CsvOpts{
-    #[arg(short,long)]
+    // 检查文件是否存在
+    #[arg(short, long, value_parser = verify_file)]
     input:String,
     #[arg(short,long,default_value="out.json")]
     output:String,
-    #[arg(short,long,default_missing_value = ",")]
+    #[arg(short,long,default_value = ",")]
     delimiter:String,
     #[arg(long, default_value_t = true)]
     header:bool,
@@ -32,4 +36,13 @@ fn main() {
     let opts:Opts = Opts::parse();
     println!("{:#?}",opts);
 
+}
+
+// 检查文件是否存在
+fn verify_file(input: &str) -> Result<String,String> {
+    if  Path::new(input).exists(){
+        Ok(input.into())
+    }else {
+        Err("file does not exist".into())
+    }
 }
