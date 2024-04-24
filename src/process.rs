2df4dda3579ff982a6956c875a2a4f3566a88d05
luoying <411426150@qq.com>
2024-04-24 23:25:21 +0800

    add process

--- src/process.rs
+++ src/process.rs
@@ -1,31 +1,46 @@
 use std::ffi::CString;
 use std::fs;
-use csv::Reader;
+use csv::{Reader,StringRecord,};
 use std::path::Path;
 use serde::{Deserialize, Serialize};
 use anyhow;
-//  Deserialize, Serialize 意思是支持序列化核反序列化
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
 
+//  Deserialize, Serialize 意思是支持序列化核反序列化
+// #[derive(Debug, Deserialize, Serialize)]
+// #[serde(rename_all = "PascalCase")]
+// pub struct Player {
+//     name: String,
+//     position: String,
+//     #[serde(rename = "DOB")]
+//     dob: String,
+//     nationality: String,
+//     #[serde(rename = "Kit Number")]
+//     kit: u8,
+// }
+//
 
 pub fn process_csv(input:&str,ouput: &str)  -> anyhow::Result<()>   {
     let mut reader = Reader::from_path(Path::new(input)).unwrap();
     let mut ret = Vec::with_capacity(128);
-    let header:StringRecord = reader.header()?.clone();
+    let header:StringRecord= reader.header()?.clone();
+
     for result in reader.deserialize() {
-        let player: Player = result.unwrap();
-        // header.iter() 使用的是gg'i't
-        ret.push(player)
+        let record:StringRecord = result.unwrap();
+        let json_value = header.iter().zip(record.iter());
+        // 等价于以下函数
+        // for i  in 0..record.len(){
+        //    let json_value = serde_json::json!({
+        //        header[i]=>record[i]
+        //    });
+        //     // println!("{}: {}", header[i], record[i]);
+        // }
+        // for (header, value ) in header.iter().zip(record.iter()) {
+        //     println!("{}: {}", header, value);
+        // }
+        // let player:StringRecord= result.unwrap();
+        // // header.iter() 使用的是迭代器
+        // let json_value = header.iter().zip(player.iter()).collect()::
+        // ret.push(player)
     }
     let json = serde_json::to_string_pretty(&ret).unwrap();
     fs::write(ouput, json)?;
